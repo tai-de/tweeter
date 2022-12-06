75ede7c8312c8d8ef3a2e073e02bebc070bae665
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2022-12-05 17:28:28 -0700

    renderTweets function created and tested, test data successfully being passed through

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,6 +14,31 @@
 
 // });
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 const createTweetElement = function(tweet) {
 
   const avatar = tweet.user.avatars;
@@ -43,3 +68,16 @@ const createTweetElement = function(tweet) {
   return $tweet;
 
 };
+
+const renderTweets = function(tweetArray) {
+  
+  for (const tweet of tweetArray) {
+    const $tweetArticle = createTweetElement(tweet)
+    $('#tweets-container').append($tweetArticle);
+  }
+  
+};
+
+$(document).ready(function() {
+renderTweets(data);
+})
