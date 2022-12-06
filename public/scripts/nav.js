93afe19cb6d2b72bd76a2551df8470dd0cd1a563
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2022-12-05 22:04:24 -0700

    add scroll to top button, nav collapse when scrolled out of view, clean up code

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -2,10 +2,42 @@ $(document).ready(function() {
 
   // Set up write new tweet button to listen for click event
 
-  $('.nav-compose-tweet').click(function() {
+  $('#nav-compose-tweet').click(function() {
 
     $('section.new-tweet').slideToggle(500);
     $('#tweet-text').focus();
+
+  });
+
+
+  // When the user scrolls down 20px from the top of the document,
+  // show the scroll to top button and collapse nav
+
+  $('#toTopButton').css('display', 'none');
+
+  $(this).scroll(function() {
+    const scrollPosition = $(window).scrollTop();
+    if (scrollPosition > 400) {
+      $('#toTopButton').css('display', 'flex');
+      $('nav').addClass('compact');
+    } else {
+      $('#toTopButton').css('display', 'none');
+      $('nav').removeClass('compact');
+    }
+  });
+
+  // Set up go to top button to listen for click event
+
+  $('#toTopButton').click(function() {
+
+    $('html, body').animate({ scrollTop: 0 }, 360);
+
+    if ($('section.new-tweet').css('display') !== 'block') {
+      $('section.new-tweet').slideToggle(500);
+    }
+    $('#tweet-text').focus();
+
+    return false;
 
   });
 
